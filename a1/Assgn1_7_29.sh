2b6c23ca9f0c639e58fedccbb695932a9cbb16d0
jena-shreyas <jenashreyas@gmail.com>
2023-01-14 12:37:06 +0530

    1.7 hopefully done

--- a1/Assgn1_7_29.sh
+++ a1/Assgn1_7_29.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 mkdir -p $2
-for ch in {a..z} ; do
- for fname in $1/*.txt ; do
-  echo $fname
- done
+for ch in {a..z} 
+do
+ touch $2/${ch}.txt
+ grep -r -h -i "^$ch" $1>$2/${ch}.txt
+ sort -o $2/${ch}.txt $2/${ch}.txt
 done
+
+
