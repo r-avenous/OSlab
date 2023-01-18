da3f648f7de3bc68fd9282469c3ae0de5272f9e4
jena-shreyas <jenashreyas@gmail.com>
2023-01-18 21:03:25 +0530

    P9 done, optimizing

--- a1/Assgn1_7_29.sh
+++ a1/Assgn1_7_29.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 mkdir -p $2
-for ch in {a..z} 
-do
+for ch in {a..z};do
  touch $2/${ch}.txt
  grep -r -h -i "^$ch" $1>$2/${ch}.txt
  sort -o $2/${ch}.txt $2/${ch}.txt
--- a1/Assgn1_9_29.sh
+++ a1/Assgn1_9_29.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+typeset -A majors students
+
+while read line;do
+    record=($(echo $line|awk '{print $1,$2}'))
+    student=${record[0]}
+    major=${record[1]}
+
+    majors[$major]=$((majors[$major]+1))
+    students[$student]=$((students[$student]+1))
+done<$1
+
+for major in ${!majors[@]};do
+    echo "$major ${majors[$major]}" 
+done|sort -t$' ' -k2rn -k1;echo
+
+count=0
+
+for student in ${!students[@]};do
+    if [ "${students[$student]}" -ge 2 ];then
+        echo $student
+    fi
+    if [ "${students[$student]}" == 1 ];then
+        count=$((count+1))
+    fi 
+done;echo $count
+
+
+
