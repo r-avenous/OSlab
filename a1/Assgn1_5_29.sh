e59965be170ce6fda1770479e39dfeeafaa0d3bf
skullVoid <atulyaankitsharma@kgpian.iitkgp.ac.in>
2023-01-18 18:50:40 +0530

    5 done

--- a1/Assgn1_5_29.sh
+++ a1/Assgn1_5_29.sh
@@ -2,4 +2,5 @@
 # find $1 -name *.py | grep -n -e \# -e \"\"\"
 # grep -n -e \# -e \"\"\" nmt-master/nmt/attention_model.py
 # awk '/#|""".*"""/ {print NR, $0}' nmt-master/nmt/attention_model.py
-grep -nR --include \*.py -e \# -e \"\"\" $1 | grep -v \"\*\#\*\"
+grep -nRE --include \*.py -e \# -e '""".' $1 | grep -vE '".*#.*"'
+# grep -nR --include \*.py -vE -e '".*#.*"' -e '^(\s)*"""(\s)*$' $1 | grep -e \# -e '"""'
