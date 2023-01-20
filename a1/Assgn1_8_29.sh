47fc5c2d93760ed0ef903ad5184f88714702e4ee
skullVoid <atulyaankitsharma@kgpian.iitkgp.ac.in>
2023-01-20 15:32:42 +0530

    8 done

--- a1/Assgn1_8_29.sh
+++ a1/Assgn1_8_29.sh
@@ -3,22 +3,26 @@ if !(test -f "main.csv");
 then
     echo "Date,Category,Amount,Name">main.csv
 fi
-# default_args=($(echo "${@: -4}" | awk '{printf("%s,%s,%s,%s\n",$1,$2,$3,$4)}' >> main.csv))
-default_args=($(echo "${@: -4}" | awk '{print $1,$2,$3,$4}'))
+args=($(echo "${@: -4}" | awk '{print $1,$2,$3,$4}'))
 
-date=${default_args[0]}
-category=${default_args[1]}
-amount=${default_args[2]}
-name=${default_args[3]}
+date=${args[0]}
+category=${args[1]}
+amount=${args[2]}
+name=${args[3]}
 
 echo "$date,$category,$amount,$name">>main.csv
 
 while getopts "c:n:s:h" opt
 do
     case $opt in 
-        c) awk -v var="$OPTARG" 'BEGIN{ FS = "," ; sum=0 } $2 == var {sum += $3} END{print sum}' main.csv;;
-        n) awk -v var="$OPTARG" 'BEGIN{ FS = "," ; sum=0 } $4 == var {sum += $3} END{print sum}' main.csv;;
-        s) sort -t ',' -k 2 main.csv;;
-        h) echo "Option h: $opt";;
+        c) awk -v var="$OPTARG" 'BEGIN{ FS = "," ; sum=0 } NR!=1 && $2 == var {sum += $3} END{print sum}' main.csv;;
+        n) awk -v var="$OPTARG" 'BEGIN{ FS = "," ; sum=0 } NR!=1 && $4 == var {sum += $3} END{print sum}' main.csv;;
+        s) case $OPTARG in
+                Date) tail -n +2 main.csv | sort -n -t ',' -k1.7 -k1.4,1.5 -k1.1,1.2 | sed '1 i\Date,Category,Amount,Name' > main2.csv;;
+                Name) tail -n +2 main.csv | sort -t ',' -k 4 | sed '1 i\Date,Category,Amount,Name' > main2.csv;;
+                Category) tail -n +2 main.csv | sort -t ',' -k 2 | sed '1 i\Date,Category,Amount,Name' > main2.csv;;
+                Amount) tail -n +2 main.csv | sort -t ',' -k 3 -n | sed '1 i\Date,Category,Amount,Name' > main2.csv;;
+            esac;;
+        h) echo "Option h";;
     esac
 done
