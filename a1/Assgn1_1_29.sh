7da9344b3446835376eb2082fcd9bc5cca231354
skullVoid <atulyaankitsharma@kgpian.iitkgp.ac.in>
2023-01-22 18:42:30 +0530

    1 shorted

--- a1/Assgn1_1_29.sh
+++ a1/Assgn1_1_29.sh
@@ -1,39 +1,53 @@
 #!/bin/bash
-function gcd() {
-	local gcd=$1
-	local a=$1
-	local b=$2
-	if [ "$b" -lt "$gcd" ]
-	then
-		gcd=$b
-	fi
-	while [ "$gcd" -ne 0 ]
-	do
-		x=`expr $a % $gcd`
-		y=`expr $b % $gcd`
-		if [ $x -eq 0 -a $y -eq 0 ] 
-		then
-			break
-		fi
-		gcd=`expr $gcd - 1`
-        done
-	echo "$gcd"     
-}
-input=$1
-answer=0
-flag=0
-while read -r line
-	do
-		line=$(echo $line | rev)
-		if [ $flag -eq 1 ] 
-		then
-			g=$(gcd "$answer" "$line")
-			answer=$(($answer*($line/$g)))
-		fi
-		if [ $flag -eq 0 ] 
-		then
-			answer=$line
-			flag=1
-		fi
-	done < "$input"
-echo "$answer"
+# function gcd() {
+# 	local gcd=$1
+# 	local a=$1
+# 	local b=$2
+# 	if [ "$b" -lt "$gcd" ]
+# 	then
+# 		gcd=$b
+# 	fi
+# 	while [ "$gcd" -ne 0 ]
+# 	do
+# 		x=`expr $a % $gcd`
+# 		y=`expr $b % $gcd`
+# 		if [ $x -eq 0 -a $y -eq 0 ] 
+# 		then
+# 			break
+# 		fi
+# 		gcd=`expr $gcd - 1`
+#         done
+# 	echo "$gcd"     
+# }
+# input=$1
+# answer=0
+# flag=0
+# while read -r line
+# 	do
+# 		line=$(echo $line | rev)
+# 		if [ $flag -eq 1 ] 
+# 		then
+# 			g=$(gcd "$answer" "$line")
+# 			answer=$(($answer*($line/$g)))
+# 		fi
+# 		if [ $flag -eq 0 ] 
+# 		then
+# 			answer=$line
+# 			flag=1
+# 		fi
+# 	done < "$input"
+# echo "$answer"
+lcm=1
+while read -r n
+do
+	x=$(echo $n | rev)
+	a=$x
+	b=$lcm
+	while [ $b -ne 0 ]; do
+		r=$((a % b))
+		a=$b
+		b=$r
+	done
+	lcm=$(((x * lcm) / a))
+done < $1
+echo $lcm
