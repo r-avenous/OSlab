6069cd3384541f9b6049785a51b81906a9aae6d8
skullVoid <atulyaankitsharma@kgpian.iitkgp.ac.in>
2023-01-22 13:16:10 +0530

    7 shorted

--- a1/Assgn1_7_29.sh
+++ a1/Assgn1_7_29.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 mkdir -p $2
 for ch in {a..z};do
- touch $2/${ch}.txt
- grep -r -h -i "^$ch" $1>$2/${ch}.txt
- sort -o $2/${ch}.txt $2/${ch}.txt
+    grep -rhi "^$ch" $1>$2/${ch}.txt
+    sort -o $2/${ch}.txt $2/${ch}.txt
 done
-
-
