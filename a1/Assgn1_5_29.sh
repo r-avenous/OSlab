e0ebbeb536af773ac6af5e8c4bafdf48bcbc8350
skullVoid <atulyaankitsharma@kgpian.iitkgp.ac.in>
2023-01-24 23:27:10 +0530

    8 final

--- a1/Assgn1_5_29.sh
+++ a1/Assgn1_5_29.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-# find $1 -name *.py | grep -n -e \# -e \"\"\"
-# grep -n -e \# -e \"\"\" nmt-master/nmt/attention_model.py
-# awk '/#|""".*"""/ {print NR, $0}' nmt-master/nmt/attention_model.py
-# grep -nRE --include \*.py -e \# -e '""".' $1 | grep -vE '".*#.*"'1
-# grep -nR --include \*.py -vE -e '".*#.*"' -e '^(\s)*"""(\s)*$' $1 | grep -e \# -e '"""'
-
-# find $1 -name '*.py' -type f -exec awk 'BEGIN{m=0} 
-# {if(m==1) {print $0; if($0 ~ /"""/) m=0; next}}
-# /^[^"]*#[^"]*$|""".*"""/ {print NR, $0; next} 
-# /"""/ {m=1; print NR, $0}' {} + > output.txt
 
 find $1 -name '*.py' -type f -print0 | xargs -0 -I {} sh -c 'echo {}; 
 awk '\''BEGIN{m=0} 
--- a1/Assgn1_8_29.sh
+++ a1/Assgn1_8_29.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if !(test -f "main.csv");
 then
-    echo "Date,Category,Amount,Name">main.csv
+    echo "date,category,amount,name">main.csv
 fi
 args=($(echo "${@: -4}" | awk '{print $1,$2,$3,$4}'))
 
@@ -12,7 +12,7 @@ name=${args[3]}
 
 echo "$date,$category,$amount,$name">>main.csv
 
-tail -n +2 main.csv | sort -n -t ',' -k1.7 -k1.4,1.5 -k1.1,1.2 | sed '1 i\Date,Category,Amount,Name' > main2.csv
+# tail -n +2 main.csv | sort -n -t ',' -k1.7 -k1.4,1.5 -k1.1,1.2 | sed '1 i\date,category,amount,name' > main.csv
 
 while getopts "c:n:s:h" opt
 do
@@ -20,11 +20,11 @@ do
         c) awk -v var="$OPTARG" 'BEGIN{ FS = "," ; sum=0 } NR!=1 && $2 == var {sum += $3} END{print sum}' main.csv;;
         n) awk -v var="$OPTARG" 'BEGIN{ FS = "," ; sum=0 } NR!=1 && $4 == var {sum += $3} END{print sum}' main.csv;;
         s) case $OPTARG in
-                Date) tail -n +2 main.csv | sort -n -t ',' -k1.7 -k1.4,1.5 -k1.1,1.2 | sed '1 i\Date,Category,Amount,Name' > main2.csv;;
-                Name) tail -n +2 main.csv | sort -t ',' -k 4 | sed '1 i\Date,Category,Amount,Name' > main2.csv;;
-                Category) tail -n +2 main.csv | sort -t ',' -k 2 | sed '1 i\Date,Category,Amount,Name' > main2.csv;;
-                Amount) tail -n +2 main.csv | sort -t ',' -k 3 -n | sed '1 i\Date,Category,Amount,Name' > main2.csv;;
+                date) tail -n +2 main.csv | sort -n -t ',' -k1.7 -k1.4,1.5 -k1.1,1.2 | sed '1 i\date,category,amount,name' | tee main.csv > /dev/null;;
+                name) tail -n +2 main.csv | sort -t ',' -k 4 | sed '1 i\date,category,amount,name' | tee main.csv > /dev/null;;
+                category) tail -n +2 main.csv | sort -t ',' -k 2 | sed '1 i\date,category,amount,name' | tee main.csv > /dev/null;;
+                amount) tail -n +2 main.csv | sort -t ',' -k 3 -n | sed '1 i\date,category,amount,name' | tee main.csv > /dev/null;;
             esac;;
-        h) printf "\nName : Splitwise \n\nUsage : Pass -c for Category and -n for Name based calculations, -s for sorting by column and -h for displaying help prompt.\n\n";;
+        h) printf "\nName : Splitwise \n\nUsage : Pass -c for category and -n for name based calculations, -s for sorting by column and -h for displaying help prompt.\n\n";;
     esac
 done
