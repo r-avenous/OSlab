0c864f829f028633cf324e7b466bde1289073e23
skullVoid <atulyaankitsharma@kgpian.iitkgp.ac.in>
2023-01-24 18:12:38 +0530

    ss

--- a1/Assgn1_9_29.sh
+++ a1/Assgn1_9_29.sh
@@ -26,21 +26,21 @@
 # done;echo $count
 
 awk '
-{ depfreq[$2] += 1 }
+{ depfreq[$2]+=1 }
 END {
     for (dep in depfreq) {
         print dep, depfreq[dep]
     }
 }' $1 | sort -t$' ' -k2rn -k1
 awk '
-{ studentfreq[$1] += 1 }
+{ studentfreq[$1]+=1 }
 END {
     for (student in studentfreq) {
-        if (studentfreq[student] >= 2) {
+        if (studentfreq[student]>=2) {
             print student
         }
         else {
-            count += 1
+            count+=1
         }
     }
     print count
