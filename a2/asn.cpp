c9bb03f1266543ffb7f83e333a447f262d30d1e0
skullVoid <atulyaankitsharma@kgpian.iitkgp.ac.in>
2023-02-01 20:15:28 +0530

    s

--- a2/asn.cpp
+++ a2/asn.cpp
@@ -18,51 +18,58 @@ vector<string> split(string s, char delim = ' ')
     return v;
 }
 pid_t pid;
+void run();
 void handlectrlc(int sig)
 {
     cout << '\n';
+    run();
+}
+void run()
+{
+    string s, inputfile, outputfile;
+    cout.flush();
+    cout << getcwd((char*)NULL, size_t(0)) << PROMPT;
+    getline(cin, s);
+    if(s == "exit") exit(0);
+    vector<string> v = split(s);
+    FILE* fpin;
+    if(v[0] == "cd")
+    {
+        chdir(v[1].c_str());
+        run();
+    }
+    for(int i=0; i<v.size(); i++)
+    {
+        if(v[i] == "<")
+        {
+            inputfile = v[i+1];
+            fpin = fopen(inputfile.c_str(), "r");
+            i++;
+        }
+        else if(v[i] == ">")
+        {
+            outputfile = v[i+1];
+            i++;
+        }
+    }
+    if(pid = fork() == 0)
+    {
+        char* args[v.size() + 1];
+        for(int i = 0; i < v.size(); i++)
+        {
+            args[i] = (char*)v[i].c_str();
+        }
+        args[v.size()] = NULL;
+        execvp(args[0], args);
+    }
+    wait(NULL);
 }
 int main()
 {
-    string s, inputfile, outputfile;
+    signal(SIGINT, handlectrlc);
     while(1)
     {
-        signal(SIGINT, handlectrlc);
-        cout << getcwd((char*)NULL, size_t(0)) << PROMPT;
-        getline(cin, s);
-        if(s == "exit") break;
-        vector<string> v = split(s);
-        FILE* fpin;
-        if(v[0] == "cd")
-        {
-            chdir(v[1].c_str());
-            continue;
-        }
-        for(int i=0; i<v.size(); i++)
-        {
-            if(v[i] == "<")
-            {
-                inputfile = v[i+1];
-                fpin = fopen(inputfile.c_str(), "r");
-                i++;
-            }
-            else if(v[i] == ">")
-            {
-                outputfile = v[i+1];
-                i++;
-            }
-        }
-        if(pid = fork() == 0)
-        {
-            char* args[v.size() + 1];
-            for(int i = 0; i < v.size(); i++)
-            {
-                args[i] = (char*)v[i].c_str();
-            }
-            args[v.size()] = NULL;
-            execvp(args[0], args);
-        }
-        wait(NULL);
+        run();
     }
     return 0;
 }
