8e499f8ed045c86f2327c449d27389f9d09ad3ea
jena-shreyas <jenashreyas@gmail.com>
2023-02-05 01:28:00 +0530

    trying to run up, down

--- a2/history.h
+++ a2/history.h
@@ -1,30 +1,12 @@
 #ifndef __HISTORY_H__
 #define __HISTORY_H__
-
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define PROMPT " [#] "
-
-typedef struct __vectorstring 
-{
-    char **data;
-    int size;
-    int capacity;
-}vectorstring;
-
-pid_t childPid;
-vectorstring cmds;
-
-void push_back(vectorstring *v, char *s);
-vectorstring split(char *s);
-void run();
-void handlectrlc(int sig);
+#include "utility.h"
+#include <readline/readline.h>
 
 // const char *history_file;
+
+int handleup(int count, int key);
+int handledown(int count, int key);
+void run();
 
 #endif
--- a2/utility.h
+++ a2/utility.h
@@ -1,5 +1,5 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef __UTILITY_H__
+#define __UTILITY_H__
 
 #include <stdio.h>
 #include <unistd.h>
