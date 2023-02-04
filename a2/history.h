086b75cb33b1cbd7329c50c09749c1eb5c36fe02
jena-shreyas <jenashreyas@gmail.com>
2023-02-04 16:21:00 +0530

    add .cmd_history

---
+++ a2/history.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PROMPT " [#] "
+
+typedef struct __vectorstring 
+{
+    char **data;
+    int size;
+    int capacity;
+}vectorstring;
+
+pid_t childPid;
+vectorstring cmds;
+
+void push_back(vectorstring *v, char *s);
+vectorstring split(char *s);
+void run();
+void handlectrlc(int sig);
+
+const char *history_file;
