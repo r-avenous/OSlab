40f987f362b351fba5e6a6f3fac0461c37653d0e
jena-shreyas <jenashreyas@gmail.com>
2023-02-14 19:46:30 +0530

    rudimentary main process (excl spawning) done

--- a3/main.cpp
+++ a3/main.cpp
@@ -3,75 +3,57 @@
 #include <vector>
 #include <fstream>
 #include <sys/shm.h>
+#define NUM_EDGES 88234
 
 using namespace std;
-
-class Graph {
-
-    int V;
-    int E;
-    vector<int>* adj;
-
-    public:
-        Graph(int V, int E) 
-        {
-            this->V = V;
-            this->E = E;
-            adj = new vector<int>[V];
-        }
-
-        void addEdge(int v, int w) 
-        {
-            adj[v].push_back(w);
-            adj[w].push_back(v);
-        }
-
-        size_t calculateGraphSize(){
-
-            int size = 0;
-            for (int v = 0; v < V; v++) 
-                size += adj[v].size();
-            
-            cout << "Graph size in bytes: " << size * sizeof(int) << endl;
-            return size * sizeof(int);
-        }
-
-        void printGraph() 
-        {
-            for (int v = 0; v < V; v++) 
-            {
-                cout << "\n Adjacency list of vertex " << v << "\n head ";
-                for (auto x : adj[v])
-                    cout << "-> " << x;
-                printf("\n");
-            }
-        }
-};
 
 int main(){
 
     const char *filename = "facebook_combined.txt";
-    Graph graph(4039, 88234);
+    char *shared_seg;
+    key_t key;
+    int shmid;
+    int memory_size = NUM_EDGES * sizeof(int) * 2 + sizeof(int); // allocate memory for graph edges, as well as number of edges
 
-    // Read the file
+    key = ftok("/tmp", 'a');                                     // generate unique key for shmget()
+    shmid = shmget(key, memory_size, IPC_CREAT | 0666);          // create shared memory segment
+    shared_seg = (char *)shmat(shmid, NULL, 0);                  // attach shared memory segment to process
+
+    // define two arrays to store 1st and 2nd nodes of graph edges; store number of edges in the last int block
+    int *num_edges = (int *)shared_seg;
+    int *first_node_arr = (int *)shared_seg + 1;
+    int *second_node_arr = (int *)shared_seg + (NUM_EDGES + 1);
+
+    // read graph edges from file
     ifstream file(filename);
     string line;
 
+    // assign pointers to the first element of the arrays
+    *num_edges = NUM_EDGES;
+    int *first_node_ptr = first_node_arr;
+    int *second_node_ptr = second_node_arr;
+
+    // read graph edges from file and write them to shared memory
     while(getline(file, line))
     {
         int node1, node2;
         sscanf(line.c_str(), "%d %d", &node1, &node2);
-        graph.addEdge(node1, node2); 
+        *first_node_ptr++ = node1;
+        *second_node_ptr++ = node2;
     }
 
-    char *myseg;
-    key_t key;
-    int shmid;
+    first_node_ptr = first_node_arr;
+    second_node_ptr = second_node_arr;
 
-    shmid = shmget(key, graph.calculateGraphSize(), IPC_CREAT | 0666);
-    myseg = (char *)shmat(shmid, NULL, 0);
+    // display graph edges from shared memory for verification
+    printf("Number of edges: %d\n", *num_edges);
+    printf("Edges in the graph: \n\n");
+    
+    for(int i = 0; i < *num_edges; i++)
+    {
+        cout << *first_node_ptr++ << " " << *second_node_ptr++ << endl;
+    }
 
-    shmdt(myseg);
-    shmctl(shmid, IPC_RMID, NULL);
+    shmdt(shared_seg);      // deallocate shared memory segment; don't delete it yet!
     return 0;
 }
