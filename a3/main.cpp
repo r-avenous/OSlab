775487745116289a16d4938b0a00cb36bf7d3669
jena-shreyas <jenashreyas@gmail.com>
2023-02-23 23:28:31 +0530

    reformat graph storage in shared memory

--- a3/main.cpp
+++ a3/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <set>
 #include <fstream>
 #include <sys/shm.h>
+#define NUM_NODES 4039
 #define NUM_EDGES 88234
 
 using namespace std;
@@ -10,50 +12,70 @@ using namespace std;
 int main(){
 
     const char *filename = "facebook_combined.txt";
+    vector<set<int>> adjList(NUM_NODES);
+    int node1, node2;
+
+    // read graph edges from file
+    ifstream file(filename);
+    string line;
+    
+    while (getline(file, line)){
+
+        sscanf(line.c_str(), "%d %d", &node1, &node2);
+        adjList[node1].insert(node2);
+        adjList[node2].insert(node1);
+    }
+
     char *shared_seg;
     key_t key;
     int shmid;
-    int memory_size = NUM_EDGES * sizeof(int) * 2 + sizeof(int); // allocate memory for graph edges, as well as number of edges
+    int memory_size = (1 + NUM_NODES + 2 * NUM_EDGES) * sizeof(int); // allocate memory for graph edges, as well as number of edges
 
     key = ftok("/tmp", 'a');                                     // generate unique key for shmget()
     shmid = shmget(key, memory_size, IPC_CREAT | 0666);          // create shared memory segment
     shared_seg = (char *)shmat(shmid, NULL, 0);                  // attach shared memory segment to process
 
-    // define two arrays to store 1st and 2nd nodes of graph edges; store number of edges in the last int block
-    int *num_edges = (int *)shared_seg;
-    int *first_node_arr = (int *)shared_seg + 1;
-    int *second_node_arr = (int *)shared_seg + (NUM_EDGES + 1);
+    int *num_nodes = (int *)shared_seg;
+    int *nbrs_cnt_ptr = num_nodes + 1;
+    int *nbrs_ptr = num_nodes + 2;
+    set<int> nbrs;
+    *num_nodes = NUM_NODES;
 
-    // read graph edges from file
-    ifstream file(filename);
-    string line;
+    // load graph into shared memory 
+    for (int i = 0; i < *num_nodes; i++){
 
-    // assign pointers to the first element of the arrays
-    *num_edges = NUM_EDGES;
-    int *first_node_ptr = first_node_arr;
-    int *second_node_ptr = second_node_arr;
+        *nbrs_cnt_ptr = adjList[i].size();
+        for (auto it = adjList[i].begin(); it != adjList[i].end(); it++){
 
-    // read graph edges from file and write them to shared memory
-    while(getline(file, line))
-    {
-        int node1, node2;
-        sscanf(line.c_str(), "%d %d", &node1, &node2);
-        *first_node_ptr++ = node1;
-        *second_node_ptr++ = node2;
+            *nbrs_ptr = *it;
+            nbrs_ptr++;
+        }
+
+        nbrs_cnt_ptr = nbrs_ptr;
+        nbrs_ptr++;
     }
 
-    first_node_ptr = first_node_arr;
-    second_node_ptr = second_node_arr;
+    num_nodes = (int *)shared_seg;
+    nbrs_cnt_ptr = num_nodes + 1;
+    nbrs_ptr = num_nodes + 2;
 
-    // display graph edges from shared memory for verification
-    printf("Number of edges: %d\n", *num_edges);
-    printf("Edges in the graph: \n\n");
-    
-    for(int i = 0; i < *num_edges; i++)
-    {
-        cout << *first_node_ptr++ << " " << *second_node_ptr++ << endl;
+    // for testing 
+    for (int i = 0; i < *num_nodes; i++){
+
+        cout << "No. of neighbors of " << i << " : " << *nbrs_cnt_ptr << endl;
+        cout << i << " : ";
+        for (auto it = adjList[i].begin(); it != adjList[i].end(); it++){
+
+            cout << *nbrs_ptr << " ";
+            nbrs_ptr++;
+        }
+
+        cout << endl;
+        nbrs_cnt_ptr = nbrs_ptr;
+        nbrs_ptr++;
     }
 
     shmdt(shared_seg);      // deallocate shared memory segment; don't delete it yet!
+    shmctl(shmid, IPC_RMID,NULL);
     return 0;
 }
