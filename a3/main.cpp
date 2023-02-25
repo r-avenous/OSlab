9c58a4c329944f28154d95b1227937af104598c9
r-avenous <atulyaankitsharma@kgpian.iitkgp.ac.in>
2023-02-25 14:05:27 +0530

    main

--- a3/main.cpp
+++ a3/main.cpp
@@ -1,81 +1,127 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <map>
 #include <set>
 #include <fstream>
 #include <sys/shm.h>
-#define NUM_NODES 4039
-#define NUM_EDGES 88234
+
+#define MAXNODES 5000
+#define MAXDEGREE 2000
+#define SETOFNODESSEGMENT 1
+#define ADJACENCYLISTSEGMENT 2
+
+inline int get_address_offset_setOfNodesSegment(int node)
+{
+    return node * (1+MAXNODES/10);
+}
+
+inline int get_address_offset_adjacencyListSegment(int node)
+{
+    return node * (1+MAXDEGREE);
+}
 
 using namespace std;
 
-int main(){
-
+int main()
+{
     const char *filename = "facebook_combined.txt";
-    vector<set<int>> adjList(NUM_NODES);
+    map<int, set<int>> adjList;
     int node1, node2;
 
     // read graph edges from file
     ifstream file(filename);
     string line;
     
-    while (getline(file, line)){
-
+    while (getline(file, line))
+    {
         sscanf(line.c_str(), "%d %d", &node1, &node2);
         adjList[node1].insert(node2);
         adjList[node2].insert(node1);
     }
 
-    char *shared_seg;
-    key_t key;
-    int shmid;
-    int memory_size = (1 + NUM_NODES + 2 * NUM_EDGES) * sizeof(int); // allocate memory for graph edges, as well as number of edges
+    int shmid_setofnodes, shmid_adjlist, *setofnodes_segment, *adjlist_segment;
+    shmid_setofnodes = shmget(SETOFNODESSEGMENT, 10 * (1 + MAXNODES/10) * sizeof(int), IPC_CREAT | 0666);          // create shared memory segment
+    shmid_adjlist = shmget(ADJACENCYLISTSEGMENT, MAXNODES * (1 + MAXDEGREE) * sizeof(int), IPC_CREAT | 0666);          // create shared memory segment
 
-    key = ftok("/tmp", 'a');                                     // generate unique key for shmget()
-    shmid = shmget(key, memory_size, IPC_CREAT | 0666);          // create shared memory segment
-    shared_seg = (char *)shmat(shmid, NULL, 0);                  // attach shared memory segment to process
+    setofnodes_segment = (int*)shmat(shmid_setofnodes, NULL, 0);                  // attach shared memory segment to process
+    adjlist_segment = (int*)shmat(shmid_adjlist, NULL, 0);                  // attach shared memory segment to process
 
-    int *num_nodes = (int *)shared_seg;
-    int *nbrs_cnt_ptr = num_nodes + 1;
-    int *nbrs_ptr = num_nodes + 2;
-    set<int> nbrs;
-    *num_nodes = NUM_NODES;
+    cout << "Number of nodes: " << adjList.size() << endl;
 
     // load graph into shared memory 
-    for (int i = 0; i < *num_nodes; i++){
-
-        *nbrs_cnt_ptr = adjList[i].size();
-        for (auto it = adjList[i].begin(); it != adjList[i].end(); it++){
-
-            *nbrs_ptr = *it;
-            nbrs_ptr++;
+    for(auto list: adjList)
+    {
+        int node = list.first;
+        int* adjlist_ptr = adjlist_segment + get_address_offset_adjacencyListSegment(node);
+        *adjlist_ptr = list.second.size();
+        for(auto nbr: list.second)
+        {
+            adjlist_ptr++;
+            *adjlist_ptr = nbr;
         }
-
-        nbrs_cnt_ptr = nbrs_ptr;
-        nbrs_ptr++;
     }
 
-    num_nodes = (int *)shared_seg;
-    nbrs_cnt_ptr = num_nodes + 1;
-    nbrs_ptr = num_nodes + 2;
-
-    // for testing 
-    for (int i = 0; i < *num_nodes; i++){
-
-        cout << "No. of neighbors of " << i << " : " << *nbrs_cnt_ptr << endl;
-        cout << i << " : ";
-        for (auto it = adjList[i].begin(); it != adjList[i].end(); it++){
-
-            cout << *nbrs_ptr << " ";
-            nbrs_ptr++;
+    // distribute nodes to 10 processes
+    int num_nodes = adjList.size();
+    int nodes_per_process = num_nodes / 10;
+    int nodes_left = num_nodes % 10;
+    int* setofnodes_ptr = setofnodes_segment;
+    int counter = 0;
+    for(int i = 0; i < 10; i++)
+    {
+        int num_nodes_in_process = nodes_per_process;
+        if(nodes_left > 0)
+        {
+            num_nodes_in_process++;
+            nodes_left--;
         }
-
-        cout << endl;
-        nbrs_cnt_ptr = nbrs_ptr;
-        nbrs_ptr++;
+        *setofnodes_ptr = num_nodes_in_process;
+        setofnodes_ptr++;
+        for(int j = 0; j < num_nodes_in_process; j++)
+        {
+            *setofnodes_ptr = counter++;
+            setofnodes_ptr++;
+        }
     }
 
-    shmdt(shared_seg);      // deallocate shared memory segment; don't delete it yet!
-    shmctl(shmid, IPC_RMID,NULL);
+    // verify the adjlist stored in shared memory
+    cout << "Adjacency list in the graph: " << endl;
+    for(int i=0; i<(int)adjList.size(); i++)
+    {
+        int* adjlist_ptr = adjlist_segment + get_address_offset_adjacencyListSegment(i);
+        int num_nbrs = *adjlist_ptr;
+        if(num_nbrs == (int)adjList[i].size()) cout << i << " (" << num_nbrs << ") :";
+        else cout << "Incorrect number of neighbors for node " << i << endl;
+        auto it = adjList[i].begin();
+        for(int j=0; j<num_nbrs; j++ , it++)
+        {
+            adjlist_ptr++;
+            if(*adjlist_ptr == *it) cout << *adjlist_ptr << " ";
+            else cout << "Incorrect neighbor for node " << i << endl;
+        }
+        cout << '\n';
+    }
+
+    // verify the set of nodes stored in shared memory
+    cout << "Set of nodes in the graph: " << endl;
+    counter = 0;
+    setofnodes_ptr = setofnodes_segment;
+    for(int i=0; i<10; i++)
+    {
+        int num_nodes_in_process = *setofnodes_ptr;
+        setofnodes_ptr++;
+        for(int j=0; j<num_nodes_in_process; j++)
+        {
+            if(counter++ != *setofnodes_ptr) cout << "Incorrect node in set of nodes" << endl;
+            setofnodes_ptr++;
+        }
+        cout << '\n';
+    }
+
+    shmdt(adjlist_segment);      // deallocate shared memory segment; don't delete it yet!
+    shmdt(setofnodes_segment);      // deallocate shared memory segment; don't delete it yet!
+    shmctl(shmid_setofnodes, IPC_RMID,NULL);
+    shmctl(shmid_adjlist, IPC_RMID,NULL);
     return 0;
 }
