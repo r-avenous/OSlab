a31123e9594ab9a39b229e6ffc2f5312b5f168be
r-avenous <atulyaankitsharma@kgpian.iitkgp.ac.in>
2023-02-26 23:56:23 +0530

    .

---
+++ a3/consumerOP.cpp
@@ -0,0 +1,214 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <map>
+#include <set>
+#include <fstream>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <queue>
+#include <algorithm>
+
+#define MAXNODES 5000
+#define MAXDEGREE 2000
+#define SETOFNODESSEGMENT 100
+#define ADJACENCYLISTSEGMENT 200
+#define PRODUCTIONSLEEP 50
+#define CONSUMPTIONSLEEP 10
+
+using namespace std;
+
+int consumerID, totalNumNodes, shmid_setofnodes, shmid_adjlist, *setofnodes_segment, *adjlist_segment;
+bool optimisation;
+char fileName[7];
+vector<set<int>> adjList(MAXNODES);
+set<int> allSources, newSources;
+vector<vector<int>> paths(MAXNODES);
+
+inline int get_address_offset_setOfNodesSegment()
+{
+    return consumerID * (1+MAXNODES/10);
+}
+inline int get_address_offset_adjacencyListSegment(int node)
+{
+    return 1 + node * (1+MAXDEGREE);
+}
+void signal_handler(int signum)
+{
+    shmdt(setofnodes_segment);
+    shmdt(adjlist_segment);
+    exit(0);
+}
+
+void printPathInFile(ofstream &fout)
+{
+    for(int i=0; i<totalNumNodes; i++)
+    {
+        if(allSources.find(i) != allSources.end())
+            continue;
+        else if(paths[i].size() == 0)
+            fout << i << " : No path\n";
+        else
+        {
+            fout << i << " <- " << paths[i][0] << " : " << paths[i].size() - 1 << " | ";
+            for (int j = 0; j < paths[i].size(); j++) 
+            {
+                fout << paths[i][j] << " ";
+            }
+            fout << endl;
+        }
+    }
+}
+
+void printDestinationPaths(const vector<int>& sources, vector<int>& dests, vector<vector<int>>& dist) 
+{
+    set<int> sourcesSet(sources.begin(), sources.end());
+    for (int i = 0; i < (int)dests.size(); i++) 
+    {
+        vector<int> path;
+        // int closestSrc = -1;
+        int minDist = 1e9;
+        for (int j = 0; j < (int)sources.size(); j++) 
+        {
+            if (dist[j][dests[i]] < minDist) 
+            {
+                // closestSrc = sources[j];
+                minDist = dist[j][dests[i]];
+            }
+        }
+        // fout << dests[i] << " <- ";
+        if (minDist == 1e9) 
+        {
+            // fout << "X : No path.\n";
+            continue;
+        }
+        int u = dests[i];
+        path.push_back(u);
+        while (allSources.find(u) == allSources.end())
+        {
+            for (int v : adjList[u]) 
+            {
+                if (dist[sources.size() - 1][v] == dist[sources.size() - 1][u] - 1) 
+                {
+                    u = v;
+                    break;
+                }
+            }
+            path.push_back(u);
+        }
+        // fout << path[path.size() - 1] << " : " << minDist << " | ";
+        // for (int j = (int)path.size() - 1; j >= 0; j--) 
+        // {
+        //     fout << path[j] << " ";
+        // }
+        // fout << endl;        
+        reverse(path.begin(), path.end());
+        if(paths[dests[i]].size() == 0)
+            paths[dests[i]] = path;
+        else if(paths[dests[i]].size() > path.size())
+            paths[dests[i]] = path;
+    }
+}
+
+void multiSourceDijkstra(const vector<int>& sources) 
+{
+    set<int> sourcesSet(sources.begin(), sources.end());
+    queue<int> q;
+    vector<bool> visited(totalNumNodes, false);
+    vector<vector<int>> dist(sources.size(), vector<int>(totalNumNodes, 1e9));
+    for (int i = 0; i < (int)sources.size(); i++) 
+    {
+        dist[i][sources[i]] = 0;
+        visited[sources[i]] = true;
+        q.push(sources[i]);
+    }
+    while (!q.empty()) 
+    {
+        int u = q.front();
+        q.pop();
+        for (int v : adjList[u]) 
+        {
+            if (!visited[v]) {
+                visited[v] = true;
+                for (int i = 0; i < (int)sources.size(); i++) {
+                    dist[i][v] = dist[i][u] + 1;
+                }
+                q.push(v);
+            }
+        }
+    }
+    vector<int> dests;
+    for (int i = 0; i < totalNumNodes; i++) 
+    {
+        if(sourcesSet.find(i) == sourcesSet.end())
+            dests.push_back(i);
+    }
+    printDestinationPaths(sources, dests, dist);
+}
+
+int main(int argc, char *argv[])
+{
+    consumerID = atoi(argv[1]);
+    sprintf(fileName, "%d.txt", consumerID);
+    cout << "Consumer " << consumerID << " is running";
+    signal(SIGINT, signal_handler);
+    optimisation = false;
+    if(argc > 2)
+    {
+        if(strcmp(argv[2], "-optimize") == 0)
+        {
+            optimisation = true;
+            cout << " optimised";
+        }
+    }
+    cout << endl;
+    
+    shmid_setofnodes = shmget(SETOFNODESSEGMENT, 10 * (1 + MAXNODES/10) * sizeof(int), IPC_CREAT | 0666);          // create shared memory segment
+    shmid_adjlist = shmget(ADJACENCYLISTSEGMENT, (1 + MAXNODES * (1 + MAXDEGREE)) * sizeof(int), IPC_CREAT | 0666);          // create shared memory segment
+
+    setofnodes_segment = (int*)shmat(shmid_setofnodes, NULL, 0);                  // attach shared memory segment to process
+    adjlist_segment = (int*)shmat(shmid_adjlist, NULL, 0);                  // attach shared memory segment to process
+
+    while(1)
+    {
+        sleep(CONSUMPTIONSLEEP);
+        ofstream file(fileName);
+        int oldSourceNum = allSources.size();
+        // get set of nodes
+        int* thisProcessSetOfNodes_segment = setofnodes_segment + get_address_offset_setOfNodesSegment();
+        int thisProcessNumNodes = *thisProcessSetOfNodes_segment;
+        // cout << "The number of nodes is " << *curAdjacencyList_segment << endl;
+        cout << "\nConsumer " << consumerID << " is consuming " << thisProcessNumNodes << " nodes:" << endl;
+        for(int i=oldSourceNum+1; i<=thisProcessNumNodes; i++)
+        {
+            // cout << *(thisProcessSetOfNodes_segment + i) << " ";
+            newSources.insert(*(thisProcessSetOfNodes_segment + i));
+            allSources.insert(*(thisProcessSetOfNodes_segment + i));
+        }
+        totalNumNodes = *adjlist_segment;
+        // get adjacency list
+        for(int i=0; i<totalNumNodes; i++)
+        {
+            int* curAdjacencyList_segment = adjlist_segment + get_address_offset_adjacencyListSegment(i);
+            int curNumNeighbors = *curAdjacencyList_segment, oldlistsize = adjList[i].size();
+            // cout << "The number of neighbors of node " << i << " is " << *curAdjacencyList_segment << endl;
+            for(int j=1+oldlistsize; j<=curNumNeighbors; j++)
+            {
+                // cout << *(curAdjacencyList_segment + j) << " ";
+                adjList[i].insert(*(curAdjacencyList_segment + j));
+            }
+            // cout << endl;
+        }
+        multiSourceDijkstra(vector<int> (newSources.begin(), newSources.end()));
+        printPathInFile(file);
+        // cout << endl;
+        file.close();
+        newSources.clear();
+        cout << "Consumer " << consumerID << " is done consuming" << endl;
+    }
+    shmdt(adjlist_segment);      // deallocate shared memory segment; don't delete it yet!
+    shmdt(setofnodes_segment);      // deallocate shared memory segment; don't delete it yet!
+    return 0;
+}
