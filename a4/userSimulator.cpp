1e393bdb5ee70c1874ce53b628aa27c0a27046ea
jena-shreyas <jenashreyas@gmail.com>
2023-03-09 23:14:07 +0530

    pls work

--- a4/userSimulator.cpp
+++ a4/userSimulator.cpp
@@ -1,6 +1,7 @@
 #include "userSimulator.hpp"
 #include "helper.hpp"
 #include <math.h>
+#include <unistd.h>
 
 extern int n;
 extern unordered_map<int, vector<int>> graph;
@@ -12,17 +13,17 @@ vector<action> pushUpdateQueue;
 pthread_mutex_t pushUpdateQueueLock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t pushUpdateQueueCond = PTHREAD_COND_INITIALIZER;
 
-#define TIMEOUT 120
+#define TIMEOUT 20
 #define PROPORTIONALITY 1
 
 void *userSimulator(void *arg)
 {
     while(true)
     {
-        out << "User simulator awake\n";
+        out << "\n\n\nUser simulator awake\n";
         // select 100 random nodes
         set<int> selectNodes;
-        while((int)selectNodes.size() < 100) selectNodes.insert(rand() % n + 1);
+        while((int)selectNodes.size() < 1) selectNodes.insert(rand() % n);
 
         out << "Selected Nodes: ";
         for(int i: selectNodes) out << i << ' ';
